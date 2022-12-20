ec75bbfe53b8b778587e765dbc5e329db0cae9a6
mark john <mark.john@editmatters.co.uk>
2022-12-20 07:07:29 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MGdmvLlHgPoRYWhPH1LgTaLLU4sgqmxj0XIOliOUtWdlmSd6xJgmhEkNF1MfQxO95COFFsqyJaqNi7iPR3bUImC00JeTBGL46',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
